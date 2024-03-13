0620fe54dacff72d2f8ab0ea282be8097af37a82
julioett <julio@estuturno.app>
2024-03-13 17:36:50 -0300

    fix

--- src/booking/model/payment-type.enum.ts
+++ src/booking/model/payment-type.enum.ts
@@ -1,8 +1,9 @@
 export enum PaymentType {
-    PAID = 'PAID',
+    RETURN = 'RETURN',
     TOTALLY = 'TOTALLY',
     PARTIAL = 'PARTIAL',
     TRIAL = 'TRIAL',
     EVALUATION = 'EVALUATION',
     PROMOTION = 'PROMOTION',
+    PAID = 'PAID'
 }
