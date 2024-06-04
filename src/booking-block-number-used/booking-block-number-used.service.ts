094b0630bda3f0f4e441d08728ff778163b89028
julioett <julio@estuturno.app>
2024-06-04 16:31:51 -0300

    create, cancel

--- src/booking-block-number-used/booking-block-number-used.service.ts
+++ src/booking-block-number-used/booking-block-number-used.service.ts
@@ -26,18 +26,15 @@ export class BookingBlockNumberUsedService {
       .whereEqualTo('date', date)
       .whereEqualTo('queueId', queueId)
       .find();
-    console.log("🚀 ~ BookingBlockNumberUsedService ~ deleteTakenBookingsBlocksByDate ~ takenBlocks:", takenBlocks);
     if (block.blocks && block.blocks.length > 0) {
         blocks = block.blocks;
     } else {
       blocks.push(block);
     }
     if (blocks && blocks.length > 0) {
-      console.log("🚀 ~ BookingBlockNumberUsedService ~ deleteTakenBookingsBlocksByDate ~ blocks:", blocks);
       for (let i = 0; i < blocks.length; i++) {
         const block = blocks[i];
         const blockToDelete = takenBlocks.filter(bck => bck.hourFrom === block.hourFrom && bck.hourTo === block.hourTo);
-        console.log("🚀 ~ BookingBlockNumberUsedService ~ deleteTakenBookingsBlocksByDate ~ blockToDelete:", blockToDelete);
         if (blockToDelete && blockToDelete.length > 0) {
           await this.bookingBlockNumbersUsedRepository.delete(blockToDelete[0].id);
         }
